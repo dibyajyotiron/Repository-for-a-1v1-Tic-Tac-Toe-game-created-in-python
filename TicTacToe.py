812ab7d5d9b99843460f301377ff2b01ed4f8068
Dibyajyoti Ghosal <32745361+dibyajyotiron@users.noreply.github.com>
2018-03-31 22:55:01 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,8 @@
 #import random
 
 board = ["0"," "," "," "," "," "," "," "," "," "]
+#The first board is just to show the user how the tic tac toe blank board looks like,it has no use in game.
+
 board1 = ["0","1","2","3","4","5","6","7","8","9"]
 
 
