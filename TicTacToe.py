da2972968748ae2b3f0bd2b603a61e8dd255b387
Dibyajyoti Ghosal <32745361+dibyajyotiron@users.noreply.github.com>
2018-03-31 23:03:07 +0530

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,24 +1,19 @@
-#import random
-
-board = ["0"," "," "," "," "," "," "," "," "," "]
-#The first board is just to show the user how the tic tac toe blank board looks like,it has no use in game.
-
-board1 = ["0","1","2","3","4","5","6","7","8","9"]
-
+board = ["0", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+board1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 def show_board():
-	print(" " * 3 + "|" + " " * 3 + "|")
+	print(" " * 3 + "|" + " " * 3 + "|" + " " * 3)
 	print(" " + board[1] + " " + "|" + " " + board[2] + " " + "|" + " " + board[3] + " ")
-	print(" " * 3 + "|" + " " * 3 + "|")
+	print(" " * 3 + "|" + " " * 3 + "|" + " " * 3)
 	print("-" * 3 + "|" + "-" * 3 + "|" + "-" * 3)
-	print(" " * 3 + "|" + " " * 3 + "|")
+	print(" " * 3 + "|" + " " * 3 + "|" + " " * 3)
 	print(" " + board[4] + " " + "|" + " " + board[5] + " " + "|" + " " + board[6] + " ")
-	print(" " * 3 + "|" + " " * 3 + "|")
+	print(" " * 3 + "|" + " " * 3 + "|" + " " * 3)
 	print("-" * 3 + "|" + "-" * 3 + "|" + "-" * 3)
-	print(" " * 3 + "|" + " " * 3 + "|")
+	print(" " * 3 + "|" + " " * 3 + "|" + " " * 3)
 	print(" " + board[7] + " " + "|" + " " + board[8] + " " + "|" + " " + board[9] + " ")
-	print(" " * 3 + "|" + " " * 3 + "|")
+	print(" " * 3 + "|" + " " * 3 + "|" + " " * 3)
 
 
 def show_board1():
@@ -33,122 +28,186 @@ def show_board1():
 	print(" " * 3 + "|" + " " * 3 + "|")
 	print(" " + board1[7] + " " + "|" + " " + board1[8] + " " + "|" + " " + board1[9] + " ")
 	print(" " * 3 + "|" + " " * 3 + "|")
-
-# show_board()
-
+# show_board1()
 
 
-
-
-
-# def player_input():
-# 	inp = ""
-# 	while not(inp == "X" or inp == "O"):
-# 		inp = input("What do you want to use, X or O : ").upper()
-# 	if inp == "X":
-# 		return "X"
-# 	else:
-# 		return "O"
-
-
-def position1():
-	pos = int(input("{}, Enter the position for X : ".format(p1)))
-	if board[pos] == " ":
-		board[pos] = "X"
-		show_board()
-	else:
-		print("This is already taken!!")
-def position2():
-	pos2 = int(input("{}, Enter the position for O: ".format(p2)))
-	if board[pos2] == " ":
-		board[pos2] = "O"
-	else:
-		print("This is already taken!!")
-
-
-def win():
+def pos():
 	while True:
-		position1()
-		position2()
-		show_board()
-		if board[1] == board[2] == board[3]=="X":
-			print("Game Over!! {} wins!!" .format(p1))
-			break
-		elif board[1] == board[2] == board[3]=="O":
-			print("Game Over!! {} wins!!".format(p2))
-			break
-		elif board[4] == board[5] == board[6]=="X":
+		pos1 = int(input("{}, Enter the position for X : ".format(p1)))
+		if board[pos1] == " ":
+			board[pos1] = "X"
+			show_board()
+		elif board[pos1] == "X":
+			print("Warning!! Don't use already occupied position!! This is already taken by X!!")
+			pos1 = int(input("{}, Enter the position for X : ".format(p1)))
+			if board[pos1] != " ":
+				print("Game over as you've given a pre-occupied position twice!!!")
+				break
+			board[pos1] = "X"
+			show_board()
+		else:
+			print("Warning!! Don't use already occupied position!! This is already taken by O!!")
+			pos1 = int(input("{}, Enter the position for X : ".format(p1)))
+			if board[pos1] != " ":
+				print("Game over as you've given a pre-occupied position twice!!!")
+				break
+			board[pos1] = "X"
+			show_board()
+		if board[1] == board[2] == board[3] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[4] == board[5] == board[6]=="O":
+		elif board[1] == board[2] == board[3] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
-		elif board[7] == board[8] == board[9]=="X":
+		elif board[4] == board[5] == board[6] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[7] == board[8] == board[9]=="O":
+		elif board[4] == board[5] == board[6] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
-		elif board[1] == board[4] == board[7]=="X":
+		elif board[7] == board[8] == board[9] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[1] == board[4] == board[7]=="O":
+		elif board[7] == board[8] == board[9] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
-		elif board[2] == board[5] == board[8]=="X":
+		elif board[1] == board[4] == board[7] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[2] == board[5] == board[8]=="O":
+		elif board[1] == board[4] == board[7] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
-		elif board[3] == board[6] == board[9]=="X":
+		elif board[2] == board[5] == board[8] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[3] == board[6] == board[9]=="O":
+		elif board[2] == board[5] == board[8] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
-		elif board[1] == board[5] == board[9]=="X":
+		elif board[3] == board[6] == board[9] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[1] == board[5] == board[9]=="O":
+		elif board[3] == board[6] == board[9] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
-		elif board[3] == board[5] == board[7]=="X":
+		elif board[1] == board[5] == board[9] == "X":
 			print("Game Over!! {} wins!!".format(p1))
 			break
-		elif board[3] == board[5] == board[7]=="O":
+		elif board[1] == board[5] == board[9] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[3] == board[5] == board[7] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[3] == board[5] == board[7] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		if tie():
+			print("Ended up in a tie!!")
+			break
+		pos2 = int(input("{}, Enter the position for O: ".format(p2)))
+		if board[pos2] == " ":
+			board[pos2] = "O"
+			show_board()
+		elif board[pos2] == "X":
+			print("Warning!! Don't use already occupied position!! This is already taken by X!!")
+			pos2 = int(input("{}, Enter the position for O: ".format(p2)))
+			if board[pos2] != " ":
+				print("Game over as you've given a pre-occupied position twice!!!")
+				break
+			board[pos2] = "O"
+			show_board()
+		else:
+			print("Warning!! Don't use already occupied position!! This is already taken by O!!")
+			pos2 = int(input("{}, Enter the position for O: ".format(p2)))
+			if board[pos2] != " ":
+				print("Game over as you've given a pre-occupied position twice!!!")
+				break
+			board[pos2] = "O"
+			show_board()
+		if board[1] == board[2] == board[3] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[1] == board[2] == board[3] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[4] == board[5] == board[6] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[4] == board[5] == board[6] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[7] == board[8] == board[9] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[7] == board[8] == board[9] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[1] == board[4] == board[7] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[1] == board[4] == board[7] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[2] == board[5] == board[8] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[2] == board[5] == board[8] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[3] == board[6] == board[9] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[3] == board[6] == board[9] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[1] == board[5] == board[9] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[1] == board[5] == board[9] == "O":
+			print("Game Over!! {} wins!!".format(p2))
+			break
+		elif board[3] == board[5] == board[7] == "X":
+			print("Game Over!! {} wins!!".format(p1))
+			break
+		elif board[3] == board[5] == board[7] == "O":
 			print("Game Over!! {} wins!!".format(p2))
 			break
 		if tie():
 			print("Ended up in a tie!!")
 			break
 
+
+def retry():
+	while True:
+		re = input("Play again?(Y or N) Y for Yes and N for No ").upper()
+		if re == "Y" :
+			for i in range(0,10):
+				board[i] = " "
+			pos()
+		else:
+			print("Bye Bye!!")
+			break
+
+
 def tie():
-	fullBoard = True
-	for i in range(1, 10):
+	full_board = True
+	for i in range(1,10):
 		if board[i] == " ":
-			fullBoard = False
-			return False
-	if fullBoard == True:
+			full_board = False
+	if full_board is True:
 		return True
 
-
-print("Welcome to this tic tac toe game!!\n This is how the demo board looks with all it's positions marked!!!")
+print("Welcome to 1V1 tic tac toe game!!\n This is how the demo board looks like with all it's positions marked!!!")
 
 show_board1()
-print("So now that you've seen the demo board-\n\nLet's begin\n\n ")
+print("Rules:\n")
+print("Rule 1: Do not enter preoccupied position twice consecutively as this will terminate the game!!\n")
+print("Rule 2: The player who goes first, will, by default have the symbol X assiciated with them!\n")
+print("Incase you want to use O, go second!!\n\n")
+print("So now that you've seen the demo board and known the rules of this game-\nLet's begin\n\n ")
 
 
-print('ENTER YOUR NAMES:\n'
-	  '(Remember if you select "X" you are player one, so name accordingly')
 p1 = input("Player 1, enter your name: ")
 p2 = input("Player 2, enter your name: ")
-
-
-position1()
-position2()
-show_board()
-win()
-
-
-
+pos()
+retry()
